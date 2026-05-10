b599cc61648861691fc0155902c01733c477522f
Viktoria Mandrik <moon171197@gmail.com>
2026-05-10 17:37:02 +0500

    lalala

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,11 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),  # добавляем API маршруты
+    path('api/v1/', include('api.urls')),
 ]
 
 if settings.DEBUG:
